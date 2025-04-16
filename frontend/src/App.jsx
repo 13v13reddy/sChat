5a3bf46f67dd6173a90fc00b6598c4d5bad6cc6f
13v13reddy <bobba.brahmareddy@gmail.com>
2025-04-16 02:22:01 -0400

    ipchange

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState, useRef } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3001');
+const socket = io('http://35.173.248.7:3001');
 
 function App() {
   const [connected, setConnected] = useState(false);
